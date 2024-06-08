db75b88ddb9efbd4d43f241633844befd7cb2259
sirsatdiv <sirsatdivya40@gmail.com>
2024-06-08 19:21:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const cors = require("cors")
-const app = exprsss()
+const app = express()
 const port = process.env.PORT || 5000
 
 let result = []
