a76d35cbf8cc251f6756200e5d38b551165cb8ee
sirsatdiv <sirsatdivya40@gmail.com>
2024-06-09 20:31:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 let result = []
+let goals = []
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -24,6 +25,24 @@ app.post("/store", (req, res) => {
      
     res.json({
         result,
+        message: "Data stored successfully!"
+    })
+})
+
+app.get("/goals", (req, res) => {
+    res.json({
+        goals,
+        message: "Data stored successfully!"
+    })
+})
+
+app.post("/storegoal", (req, res) => {
+    const { g_name, category, desc } = req.body
+
+    result.push({ g_name, category, desc })
+     
+    res.json({
+        goals,
         message: "Data stored successfully!"
     })
 })
